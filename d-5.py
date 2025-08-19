442ba076e32a69785f748e671c32696af0ca4907
koto-1207 <koto0429@gmail.com>
2025-08-19 13:41:09 +0900

    submit d-5

--- d-5.py
+++ d-5.py
@@ -9,19 +9,19 @@ class MyCounterV2:
 
 
 counter1 = MyCounterV2(value=0, step=1)
-print(counter1.value)  # 0
+print(counter1.value)
 
 counter1.count_up()
-print(counter1.value)  # 1
+print(counter1.value)
 
 counter1.count_up()
-print(counter1.value)  # 2
+print(counter1.value)
 
 counter2 = MyCounterV2(value=0, step=3)
-print(counter2.value)  # 0
+print(counter2.value)
 
 counter2.count_up()
-print(counter2.value)  # 3
+print(counter2.value)
 
 counter2.count_up()
-print(counter2.value)  # 6
+print(counter2.value)
